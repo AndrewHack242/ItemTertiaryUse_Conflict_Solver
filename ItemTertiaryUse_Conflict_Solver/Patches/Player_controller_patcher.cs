4ac38d7bec933455e55940a9ac5c469e9ea8b521
AndrewHack242 <andrew.hack242@gmail.com>
2024-04-27 09:57:34 -0600

    Add project files.

---
+++ ItemTertiaryUse_Conflict_Solver/Patches/Player_controller_patcher.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace ItemTertiaryUse_Conflict_Solver.Patches
+{
+    [HarmonyPatch(typeof(PlayerControllerB))]
+    internal class Player_controller_patcher
+    {
+        [HarmonyPatch("ItemTertiaryUse_performed")]
+        [HarmonyPrefix]
+        static bool patch_bad_interactions(ref InteractTrigger ___hoveringOverTrigger)
+        {
+            if (___hoveringOverTrigger != null)
+            {
+                string interact_key = "interact_default", use_key = "use_default";
+                foreach (var binding in IngamePlayerSettings.Instance.playerInput.actions.FindAction("Interact").bindings)
+                {
+                    if (binding.path.Contains("Keyboard"))
+                    {
+                        interact_key = binding.overridePath;
+                    }
+                }
+                foreach (var binding in IngamePlayerSettings.Instance.playerInput.actions.FindAction("ItemTertiaryUse").bindings)
+                {
+                    if (binding.path.Contains("Keyboard"))
+                    {
+                        use_key = binding.overridePath;
+                    }
+                }
+                ItemTertiaryUse_conflict_solver_base.LogInfo("keybind comparison: " + interact_key + ", " + use_key);
+                if (interact_key == use_key)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ ItemTertiaryUse_Conflict_Solver/Plugin.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ItemTertiaryUse_Conflict_Solver
+{
+    [BepInPlugin(mod_GUID, mod_name, mod_version)]
+    public class ItemTertiaryUse_conflict_solver_base : BaseUnityPlugin
+    {
+        private const string mod_GUID = "Hackattack242.ItemTertiaryUse_Conflict_Solver";
+        private const string mod_name = "ItemTertiaryUse Conflict Solver";
+        private const string mod_version = "1.0.0";
+
+        private readonly Harmony harmony = new Harmony(mod_GUID);
+
+        private static ItemTertiaryUse_conflict_solver_base Instance;
+
+        private ManualLogSource logger;
+
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+
+            logger = BepInEx.Logging.Logger.CreateLogSource(mod_name);
+
+            logger.LogInfo(mod_name + " has awoken");
+
+            harmony.PatchAll();
+        }
+
+        internal static void LogDebug(string message)
+        {
+            Instance.Log(message, (LogLevel)32);
+        }
+
+        internal static void LogInfo(string message)
+        {
+            Instance.Log(message, (LogLevel)16);
+        }
+
+        internal static void LogWarning(string message)
+        {
+            Instance.Log(message, (LogLevel)4);
+        }
+
+        internal static void LogError(string message)
+        {
+            Instance.Log(message, (LogLevel)2);
+        }
+
+        internal static void LogError(Exception ex)
+        {
+            Instance.Log(ex.Message + "\n" + ex.StackTrace, (LogLevel)2);
+        }
+
+        private void Log(string message, LogLevel logLevel)
+        {
+            ((ItemTertiaryUse_conflict_solver_base)this).logger.Log(logLevel, (object)message);
+        }
+
+    }
+}
